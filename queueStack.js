e96006deb9387411c3dd5e2e1a9fa2e652a55d7a
Michael Chiang <michiang@ucsd.edu>
2017-02-13 20:56:43 -0800

    Add queueStack

---
+++ queueStack.js
@@ -0,0 +1,41 @@
+//Write a stack. Once you’re done, implement a queue using two stacks. Do not create a storage array for your queue.
+
+var Stack = function() {
+  var storage = [];
+
+  this.push = function(value){
+    storage.push(value);
+  };
+
+  this.pop = function(){
+    return storage.pop();
+  };
+
+  this.size = function(){
+    return storage.length;
+  };
+};
+
+var Queue = function() {
+
+  var inbox = new Stack();
+  var outbox = new Stack();
+
+  this.enqueue = function(value){
+    while(outbox.size() > 0) {
+      inbox.push(outbox.pop())
+    }
+    inbox.push(value)
+  };
+
+  this.dequeue = function(){
+  while (inbox.size() > 0) {
+      outbox.push(inbox.pop());
+    }
+    return outbox.pop();
+  };
+
+  this.size = function(){
+    return inbox.size() + outbox.size();
+  };
+};
