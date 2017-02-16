e24c6810e759b8a809dc12302dadbb566c9a2a9f
Michael Chiang <michiang@ucsd.edu>
2017-02-16 09:36:01 -0800

    Add isSubsetOf

---
+++ isSubsetOf.js
@@ -0,0 +1,14 @@
+/*
+Make an array method that can return whether or not a context array is a subset of an input array. 
+To simplify the problem, you can assume that both arrays will contain only strings.
+*/
+
+Array.prototype.isSubsetOf = function(array) {
+  let subset = true;
+  this.forEach(function(element) {
+    if (!array.includes(element)) {
+      subset = false;
+    }
+  });
+  return subset;
+};
